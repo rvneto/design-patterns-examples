c2f4eb085f37e95c560858f64e82bb4c1c8d6688
roberto <rvneto.rs@gmail.com>
2024-08-13 21:21:57 -0300

    Estruturação das pastas

--- src/test/java/com/roberto/designpatterns/ApplicationTests.java
+++ src/test/java/com/roberto/designpatterns/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DesignPatternsApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
